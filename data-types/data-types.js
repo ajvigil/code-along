6479acdadad6c2892a99b080ba8231610ca08b90
Alicia Vigil <avigil229@cnm.edu>
2021-10-06 10:20:40 -0600

    added functions to js

---
+++ data-types/data-types.js
@@ -0,0 +1,15 @@
+//String
+const name = 'Fred'
+const anotherName = "jim"
+const yetAnotherName = `Mary and ${name}`
+//number
+const age = 18
+
+const zipcode = '87109'
+
+const cars = ['ford', 'dodge', 'toyota']
+const empty = []
+console.log(cars[2])
+
+//Object ......JSON
+const person = {name: 'Fred', age:'18'}
