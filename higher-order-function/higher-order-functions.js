418ed2b3d3b61877c0b8b5791648a9689d0d6d17
Alicia Vigil <avigil229@cnm.edu>
2021-10-25 14:11:07 -0600

    added higher-order-functions.js

---
+++ higher-order-function/higher-order-functions.js
@@ -0,0 +1,14 @@
+//simplification of a process we don't have to know about - but we can still use it
+// aka driving a car, we don't know how it works but we still use it
+
+//functions that are called upon other functions are higher order functions
+
+//the basics of arrow functions
+
+// let func = (arg1, arg2, arg3) => expression
+
+//SAME AS
+
+// let func = function (arg1, arg2, arg3) {
+//     return expression;
+// }
