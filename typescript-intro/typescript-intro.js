9d58697f9116a2a0ad9714f66c05d9098d2e342f
Alicia Vigil <avigil229@cnm.edu>
2021-11-11 10:00:36 -0700

    intro to typescript

---
+++ typescript-intro/typescript-intro.js
@@ -0,0 +1,116 @@
+console.log(Infinity / -0)
+
+console.log([0, -1, -2].sort())
+
+const array = [
+    {
+        "userId": 1,
+        "id": 1,
+        "company": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "company": "qui est esse",
+        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "company": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {
+        "userId": 1,
+        "id": 4,
+        "company": "eum et est occaecati",
+        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+        "userId": 1,
+        "id": 5,
+        "company": "nesciunt quas odio",
+        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    },
+    {
+        "userId": 1,
+        "id": 6,
+        "company": "dolorem eum magni eos aperiam quia",
+        "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+    },
+    {
+        "userId": 1,
+        "id": 7,
+        "company": "magnam facilis autem",
+        "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+    },
+    {
+        "userId": 1,
+        "id": 8,
+        "company": "dolorem dolore est ipsam",
+        "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
+    },
+    {
+        "userId": 1,
+        "id": 9,
+        "company": "nesciunt iure omnis dolorem tempora et accusantium",
+        "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
+    },
+    {
+        "userId": 1,
+        "id": 10,
+        "company": "optio molestias id quia eum",
+        "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
+    }
+]
+
+const doh = array.map(index => {
+    index.company = undefined
+    return index
+})
+
+console.log(doh)
+
+//this
+const testThis = {
+    prop: 42,
+    func: function () {
+        return this.prop
+    },
+};
+
+console.log(testThis.func())
+
+
+function makeAdder(x) {
+    return function sum (y) {
+        return x + y;
+    }
+}
+
+let add10 = makeAdder(10)
+
+console.log(add10(7))
+
+//class capitalize names
+const Greater = (function () {
+    function Greater(greeting) {
+        this.greeting = greeting
+    }
+    Greater.prototype.getGreeting = function () {
+        return (this.greeting)
+    }
+    Greater.prototype.setGreeting = function (newGreeting) {
+        this.greeting = newGreeting
+    }
+
+    Greater.prototype.greet = function () {
+        return "Hello, " + this.greeting
+    }
+
+    return Greater
+
+})
+
+const greeter1 = Greater.Gre
